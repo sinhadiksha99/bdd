659fcfe953df6371233ed9ff5155bf53956fd0c0
Diksha <sinhadiksha99.com>
2025-07-08 22:21:47 +0530

    uncomment code to run using testng runner also use one of the runner config in pom.xml as it may lead to inconsistency

--- src/test/java/cucumberOptions/TestNgTestRunner.java
+++ src/test/java/cucumberOptions/TestNgTestRunner.java
@@ -1,10 +1,9 @@
+// package cucumberOptions;
 
-package cucumberOptions;
+// import io.cucumber.testng.AbstractTestNGCucumberTests;
+// import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+// @CucumberOptions(features = "src/test/java/features/Login.feature", glue =  "stepDefinitions")
+// public class TestNgTestRunner extends AbstractTestNGCucumberTests{
 
-@CucumberOptions(features = "src/test/java/features/Login.feature", glue =  "stepDefinitions")
-public class TestNgTestRunner extends AbstractTestNGCucumberTests{
-
-}
+// }
