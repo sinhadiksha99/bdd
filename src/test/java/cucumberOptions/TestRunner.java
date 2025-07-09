5f4bc163ff8e0cfde63fb140b4970d3bae961ccb
Diksha <sinhadiksha99.com>
2025-07-09 20:56:46 +0530

    add attribute notes here

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -11,7 +11,8 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "src/test/java/features", 
                     glue = "stepDefinitions", 
                     stepNotifications = false, 
-                    tags = "@PortalTest") // to run a specific tag)
+                    tags = "@PortalTest",
+                    plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json","junit:target/cukes.xml"}) // to run a specific tag)
 public class TestRunner {
 
 }
@@ -32,4 +33,17 @@ public class TestRunner {
  * When User login into application with username "hello" and password "world"
  * Then Home page is populated
  * And Cards are displayed "true"
+ */
+/*
+ * dryRun = true is used to check if all the step definitions are
+ * implemented or not. It will not run the tests but will check if all the steps
+ * are defined in the step definition file. If any step is not defined, it will
+ * give an error message with the missing step definition.
+ * dryRun = false is used to run the tests. It will execute the tests and give
+ * the results.
+ */
+/* monochrome = true is used to make the console output more readable by
+ * removing hashcode etc. It will not affect the test execution but will
+ * improve the readability of the console output.
+ * in a plain text format without any additional styling.
  */
